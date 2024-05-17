d65a25ec93186aa148b973eb4f4a4c89e99ffd34
YunHak Lee <lyh990408@gmail.com>
2024-05-18 04:37:56 +0900

    [Setting] #7 - 프로젝트 기초 세팅
    컬러, 폴더링, 앱아이콘&이름, 17.0 세팅. 폰트 적용을 위한 작업 필요. 추후 세팅 이슈에서 다시 진행할 예정

---
+++ STOD/STOD/Resource/Constant/StringLiterals.swift
@@ -0,0 +1,12 @@
+//
+//  StringLiterals.swift
+//  STOD
+//
+//  Created by 이윤학 on 5/18/24.
+//
+
+import Foundation
+
+enum StringLiterals {
+    
+}
---
+++ STOD/STOD/Source/User Interface/CustomView/CustomView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomView.swift
+//  STOD
+//
+//  Created by 이윤학 on 5/18/24.
+//
+
+import SwiftUI
+
+struct CustomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomView()
+}
