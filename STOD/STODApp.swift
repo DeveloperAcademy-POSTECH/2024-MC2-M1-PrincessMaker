89ed238a843f79c4e88e56b442f8499353195b97
darongzzang <babsalice@naver.com>
2024-05-19 00:52:04 +0900

    Initial Commit

---
+++ STOD/STODApp.swift
@@ -0,0 +1,32 @@
+//
+//  STODApp.swift
+//  STOD
+//
+//  Created by 김이예은 on 5/19/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct STODApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
