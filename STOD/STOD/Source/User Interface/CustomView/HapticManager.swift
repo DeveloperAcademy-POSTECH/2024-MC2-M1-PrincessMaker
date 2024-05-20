ef8a90dc9b0f18e0589136bb4ccfb0470162b045
YunHak Lee <lyh990408@gmail.com>
2024-05-20 23:38:06 +0900

    [Design] #23 - 스크롤 가능하게 해제, 탭 하는 경우 햅틱 설정 완료.

---
+++ STOD/STOD/Source/User Interface/CustomView/HapticManager.swift
@@ -0,0 +1,23 @@
+//
+//  HapticManager.swift
+//  STOD
+//
+//  Created by 이윤학 on 5/20/24.
+//
+
+import SwiftUI
+
+class HapticManager {
+    static let instance = HapticManager()
+    private init() {}
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
--- STOD/STOD/Source/User Interface/Home/CategorySection.swift
+++ STOD/STOD/Source/User Interface/Home/CategorySection.swift
@@ -45,13 +45,29 @@ struct CategorySection: View {
                         }
                     }
                     .onChange(of: selectedCategory) { oldValue, newValue in
-                        withAnimation {
-                            value.scrollTo(newValue, anchor: .center)
+                        let oldIndex = MainCategory.allCases.firstIndex(of: oldValue ?? .recent) ?? 0
+                        
+                        let newIndex = MainCategory.allCases.firstIndex(of: newValue ?? .accessory) ?? 4
+                        
+                        let duration = Double(abs(newIndex - oldIndex))*0.2
+                        
+                        withAnimation(.easeInOut(duration: duration)) {
                             // 선택된 카테고리로 스크롤
+                            value.scrollTo(newValue, anchor: .center)
+                            
+                            let count = abs(newIndex - oldIndex)
+                            for i in 0..<count {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + (0.1 * Double(i))) {
+                                    HapticManager.instance.impact(style: .rigid)
+                                }
+                            }
+                            
                         }
+                        
                     }
                 }
             }
+            //.scrollDisabled(true)
             .frame(height: 80)
         }
     }
