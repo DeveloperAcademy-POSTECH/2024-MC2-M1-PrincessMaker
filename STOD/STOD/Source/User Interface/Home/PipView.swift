fd00a5367ff1be43c91a33783b67a6b7629fac0a
TakeMos <kangh0423@gmail.com>
2024-05-26 03:53:30 +0900

    [Feat] #35- pip화면에 선택한 옷의 사이즈 표시

---
+++ STOD/STOD/Source/User Interface/Home/PipView.swift
@@ -0,0 +1,36 @@
+//
+//  PipView.swift
+//  STOD
+//
+//  Created by kim kanghyeok on 5/26/24.
+//
+
+import SwiftUI
+
+struct PipView: View {
+    @Binding var selectedCloth: Cloth
+    
+    var body: some View {
+        ZStack {
+            Image(selectedCloth.mainPhotoPath!)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 200)
+            VStack {
+                Spacer()
+                HStack {
+                    ZStack {
+                        Circle()
+                            .frame(width:35, height: 35)
+                            .foregroundStyle(.black)
+                            .opacity(0.4)
+                        Text(selectedCloth.size)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(15)
+                    Spacer()
+                }
+            }
+        }
+    }
+}
