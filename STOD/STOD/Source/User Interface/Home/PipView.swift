f7194094681b03d3b273f6c27f77d910a891e864
TakeMos <kangh0423@gmail.com>
2024-05-26 18:47:37 +0900

    [Fix] #13 - pip관련 에러처리

--- STOD/STOD/Source/User Interface/Home/PipView.swift
+++ STOD/STOD/Source/User Interface/Home/PipView.swift
@@ -9,29 +9,32 @@ import SwiftUI
 
 struct PipView: View {
     @Binding var selectedCloth: Cloth?
-    
+
     var body: some View {
         ZStack {
             if let uiImage = selectedCloth?.numericalUIImage {
                 Image(uiImage: uiImage)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 300, height: 200)
+//                    .resizable()
+                    .aspectRatio(contentMode: .fit)
                 VStack {
                     Spacer()
                     HStack {
                         ZStack {
                             Circle()
-                                .frame(width:35, height: 35)
+                                .frame(width:100, height: 100)
                                 .foregroundStyle(.black)
                                 .opacity(0.4)
                             Text(selectedCloth?.size ?? "")
+                                .font(.largeTitle)
                                 .foregroundStyle(.white)
                         }
                         .padding(15)
                         Spacer()
                     }
                 }
+            } else {
+                Image(.characterHome)
+                    .frame(width: 1280, height: 640)
             }
         }
     }
