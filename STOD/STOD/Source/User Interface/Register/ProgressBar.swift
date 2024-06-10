b06480085f613d726a489e2684598703bb740a14
TakeMos <kangh0423@gmail.com>
2024-06-10 20:27:50 +0900

    [Fix] #58 - 스크롤 뷰에 프로그레스바 텍스트가 가려지는 버그 수정

--- STOD/STOD/Source/User Interface/Register/ProgressBar.swift
+++ STOD/STOD/Source/User Interface/Register/ProgressBar.swift
@@ -8,13 +8,12 @@ struct ProgressBar: View {
         
         HStack(spacing: 0) {
             ForEach(0..<StringLiterals.Progress.allCases.count - 1) { stage in
-                Circle()
-                    .zIndex(1)
-                    .frame(width: 9, height: 9)
-                    .foregroundStyle(.accent)
-                    .overlay(progressStage <= stage ? Circle().frame(width: 14, height: 14).foregroundStyle(progressStage == stage ? .accent : Color(hex: "#2B2B2B")).opacity(progressStage == stage ? 0.5 : 1) : nil)
-                    .overlay(Text(StringLiterals.Progress.allCases[stage].rawValue).font(.StodBody).offset(y: 30).frame(width:52).foregroundStyle(progressStage == stage ? .white : Color(hex: "#2B2B2B")))
-                
+                    Circle()
+                        .zIndex(1)
+                        .frame(width: 9, height: 9)
+                        .foregroundStyle(.accent)
+                        .overlay(progressStage <= stage ? Circle().frame(width: 14, height: 14).foregroundStyle(progressStage == stage ? .accent : Color(hex: "#2B2B2B")).opacity(progressStage == stage ? 0.5 : 1) : nil)
+                        .overlay(Text(StringLiterals.Progress.allCases[stage].rawValue).font(.StodBody).offset(y: 30).frame(width: 52).foregroundStyle(progressStage == stage ? .white : Color(hex: "#2B2B2B")))
                 Rectangle()
                     .frame(width: UIScreen.main.bounds.width / 4, height: 1)
                     .foregroundStyle(progressStage > stage ? .accent : Color(hex: "#2B2B2B"))
@@ -25,7 +24,8 @@ struct ProgressBar: View {
                 .overlay(progressStage <= 4 ? Circle().frame(width: 14, height: 14).foregroundStyle(progressStage >= 3 ? .accent : Color(hex:"#2B2B2B")).opacity(progressStage >= 3 ? 0.5 : 1) : nil)
                 .overlay(Text(StringLiterals.Progress.allCases[3].rawValue).font(.StodBody).offset(y: 30).frame(width:52).foregroundStyle(progressStage >= 3 ? .white : Color(hex: "#2B2B2B")))
         }
-        .padding(EdgeInsets(top: 24, leading: 0, bottom: 12, trailing: 0))
+        .padding(EdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0))
+        .frame(height: 60, alignment: .top)
     }
 }
 
