1f658aa731f77f8426cca2e307ac453c75495518
TakeMos <kangh0423@gmail.com>
2024-06-12 15:16:29 +0900

    [Fix] #58 - 프로그레스바 스크롤 뷰 사이 간격 수정

--- STOD/STOD/Source/User Interface/Register/ProgressBar.swift
+++ STOD/STOD/Source/User Interface/Register/ProgressBar.swift
@@ -12,20 +12,20 @@ struct ProgressBar: View {
                         .zIndex(1)
                         .frame(width: 9, height: 9)
                         .foregroundStyle(.accent)
-                        .overlay(progressStage <= stage ? Circle().frame(width: 14, height: 14).foregroundStyle(progressStage == stage ? .accent : Color(hex: "#2B2B2B")).opacity(progressStage == stage ? 0.5 : 1) : nil)
-                        .overlay(Text(StringLiterals.Progress.allCases[stage].rawValue).font(.StodBody).offset(y: 30).frame(width: 52).foregroundStyle(progressStage == stage ? .white : Color(hex: "#2B2B2B")))
+                        .overlay(progressStage <= stage ? Circle().frame(width: 14, height: 14).foregroundStyle(progressStage == stage ? .accent : .stodGray200).opacity(progressStage == stage ? 0.5 : 1) : nil)
+                        .overlay(Text(StringLiterals.Progress.allCases[stage].rawValue).font(.StodBody).offset(y: 30).frame(width: 52).foregroundStyle(progressStage == stage ? .white : .stodGray200))
                 Rectangle()
                     .frame(width: UIScreen.main.bounds.width / 4, height: 1)
-                    .foregroundStyle(progressStage > stage ? .accent : Color(hex: "#2B2B2B"))
+                    .foregroundStyle(progressStage > stage ? .accent : .stodGray200)
             }
             Circle()
                 .frame(width: 9, height: 9)
                 .foregroundStyle(.accent)
                 .overlay(progressStage <= 4 ? Circle().frame(width: 14, height: 14).foregroundStyle(progressStage >= 3 ? .accent : Color(hex:"#2B2B2B")).opacity(progressStage >= 3 ? 0.5 : 1) : nil)
-                .overlay(Text(StringLiterals.Progress.allCases[3].rawValue).font(.StodBody).offset(y: 30).frame(width:52).foregroundStyle(progressStage >= 3 ? .white : Color(hex: "#2B2B2B")))
+                .overlay(Text(StringLiterals.Progress.allCases[3].rawValue).font(.StodBody).offset(y: 30).frame(width:52).foregroundStyle(progressStage >= 3 ? .white : .stodGray200))
         }
-        .padding(EdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0))
-        .frame(height: 60, alignment: .top)
+        .padding(.top, 24)
+        .frame(height: 68, alignment: .top)
     }
 }
 
